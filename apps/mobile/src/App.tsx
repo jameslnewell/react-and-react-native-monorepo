b12a8816817566c31cd02badb7ab96ffa8712466
James Newell <james.newell@spaceship.com.au>
2020-09-14 09:14:06 +1000

    convert app to typescript

---
+++ apps/mobile/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native';
+
+export const App: React.FC = () => {
+  return (
+    <>
+      <SafeAreaView>
+        <Text>Hello World!</Text>
+      </SafeAreaView>
+    </>
+  );
+};
